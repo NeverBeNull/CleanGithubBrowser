c9ac3e681b6ec57209907385cb1db48f19d0943a
mineui <alsdmlek4@gmail.com>
2021-11-29 15:08:03 +0900

    Apply Timber

--- app/src/main/java/com/neverbenull/cleangithubbrowser/presentation/MainApplication.kt
+++ app/src/main/java/com/neverbenull/cleangithubbrowser/presentation/MainApplication.kt
@@ -1,8 +1,18 @@
 package com.neverbenull.cleangithubbrowser.presentation
 
 import android.app.Application
+import com.neverbenull.cleangithubbrowser.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }
